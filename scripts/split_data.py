09f93f564b598ffbf591aff39426d566ae36da04
callmeeric5 <callmeew5@gmail.com>
2024-04-09 17:18:15 +0200

    add data split

---
+++ scripts/split_data.py
@@ -0,0 +1,27 @@
+import os
+import pandas as pd
+
+
+def split_dataset():
+    output_folder = '../data_raw/Ingestion'
+    num_files = 20
+    dataset_path = '../data_raw/data_with_errors.csv'
+    try:
+        df = pd.read_csv(dataset_path)
+    except FileNotFoundError:
+        print(f"Error: Dataset file '{dataset_path}' not found.")
+        return
+
+    os.makedirs(output_folder, exist_ok=True)
+
+    num_rows_per_file = len(df) // num_files
+
+    for i in range(num_files):
+        start_idx = i * num_rows_per_file
+        end_idx = (i + 1) * num_rows_per_file if i < num_files - 1 else len(df)
+        subset = df.iloc[start_idx:end_idx]
+        subset.to_csv(os.path.join(output_folder, f"data_{i + 1}.csv"), index=False)
+
+    print(f"Dataset split into {num_files} files in the '{output_folder}' folder.")
+
+
---
+++ scripts/testpast.py
@@ -0,0 +1,98 @@
+# # from scripts.make_db import db_engine
+# # from sqlalchemy import text
+# #
+# # # Obtain the session
+# # session_data = db_engine()
+# # session = session_data["Session"]()
+# #
+# # # Define the SQL query
+# # sql_query = 'SELECT * FROM "Prediction_Table" WHERE source = \'webapp\';'
+# #
+# #
+# # # Execute the query
+# # result = session.execute(text(sql_query))
+# #
+# # # Fetch the results
+# # rows = result.fetchall()
+# #
+# # # Process the results as needed
+# # for row in rows:
+# #     # Do something with each row
+# #     print(row)
+#
+# from scripts.make_predticiton_table import Prediction_Table
+# from scripts.make_db import db_engine
+# from sqlalchemy.orm import sessionmaker
+# #
+# session_data = db_engine()
+# past_predictions = []
+# Session = session_data["Session"]
+# session = Session()
+# query = session.query(Prediction_Table)
+#
+# query = query.filter(Prediction_Table.source == "webapp")
+# query = query.filter(Prediction_Table.created_at >= '2024-04-02')
+#
+# query = query.filter(Prediction_Table.created_at <= '2024-04-03')
+# result = query.all()
+# for item in result:
+#                 prediction_dict = {
+#                     "id": item.id,
+#                     "occupation": item.occupation,
+#                     "marital_status": item.marital_status,
+#                     "product_category_1": item.product_category_1,
+#                     "product_category_2": item.product_category_2,
+#                     "product_category_3": item.product_category_3,
+#                     "age": item.age,
+#                     "gender": item.gender,
+#                     "city_category": item.city_category,
+#                     "stay_in_current_city_years": item.stay_in_current_city_years,
+#                     "purchase": item.purchase,
+#                     "source": item.source,
+#                     "created_at": item.created_at,
+#                 }
+#                 past_predictions.append(prediction_dict)
+#
+# session.close()
+# print(past_predictions)
+import requests
+
+
+# def test_get_predictions():
+#     # Define a list of filter options to test
+#     filter_options = ["webapp", "scheduled", "all"]
+#
+#     # Iterate over each filter option
+#     for filter_option in filter_options:
+#         # Make a GET request to the API endpoint with the current filter option
+#         response = requests.get(f"http://localhost:8000/past-predictions?filter_option={filter_option}")
+#
+#         # Check if the request was successful (status code 200)
+#         if response.status_code == 200:
+#             # Print the response data
+#             print(f"Filter Option: {filter_option}")
+#             print("Response:")
+#             print(response.json())
+#         else:
+#             print(f"Failed to get predictions for filter option {filter_option}. Error:", response.status_code)
+# import requests
+#
+# url = "http://localhost:8000/past-predictions"
+# params = {
+#     "filter_option": "webapp",
+#     "start_date": "2024-04-02",
+#     "end_date": "2024-04-03",
+# }
+#
+# response = requests.get(url, params=params)
+#
+# print(response.json())
+
+# Run the test
+# test_get_predictions()
+
+# from scripts.get_past_prediction import get_past_predictions
+# res = get_past_predictions('webapp','2024-04-02','2024-04-03')
+# print(res)
+from scripts.split_data import split_dataset
+split_dataset()
